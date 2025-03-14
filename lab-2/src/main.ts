9e1241a7f15ec1a4702e600e78f03dec2542ecd8
Ramon-denv <ramonmendespeixoto008@gmail.com>
2025-03-14 20:27:37 -0300

    atualização no csv

--- lab-2/src/main.ts
+++ lab-2/src/main.ts
@@ -1,10 +1,13 @@
 import { GitService } from './service/git.service';
+import { saveRepositoriesToCsv } from './requisitos/requisito-1';
 
 const init = async () => {
-  const gitService = new GitService();
-  const repositories = await gitService.getRepositories(10);
-
-  console.log(repositories);
+  try {
+    const gitService = new GitService();
+    const repositories = await gitService.getRepositories(10);
+    await saveRepositoriesToCsv();
+  } catch (error) {
+    console.error('Erro ao salvar os repositórios no CSV:', error);
+  }
 };
-
 init();
--- lab-2/src/requisitos/requisito-1.ts
+++ lab-2/src/requisitos/requisito-1.ts
@@ -1,28 +1,29 @@
 import { GitService } from '../service/git.service';
 import { createCsv } from '../utils/create-csv.util';
 
-async function saveRepositoriesToCsv() {
-  const gitService = new GitService();
-  const quantity = 10;
-  const repositories = await gitService.getRepositories(quantity);
+export async function saveRepositoriesToCsv() {
+  try {
+    const gitService = new GitService();
+    const quantity = 10;
+    const repositories = await gitService.getRepositories(quantity);
 
-  const formattedData = repositories.map((repo: any) => ({
-    name: repo.name,
-    owner: repo.owner.login,
-    primaryLanguage: repo.primaryLanguage?.name || 'Unknown',
-    stargazerCount: repo.stargazerCount,
-    createdAt: repo.createdAt,
-    updatedAt: repo.updatedAt,
-    pullRequests: repo.pullRequests.totalCount,
-    mergedPullRequests: repo.mergedPullRequests?.totalCount || 0,
-    issues: repo.issues.totalCount,
-    closedIssues: repo.closedIssues?.totalCount || 0,
-    releases: repo.releases.totalCount,
-  }));
-
-  const filePath = '../csv/repositorio-1.csv';
-
-  createCsv(formattedData, filePath);
+    // Formatação dos dados
+    const formattedData = repositories.map((repo: any) => ({
+      name: repo.name,
+      owner: repo.owner.login,
+      primaryLanguage: repo.primaryLanguage?.name || 'Unknown',
+      stargazerCount: repo.stargazerCount,
+      createdAt: repo.createdAt,
+      updatedAt: repo.updatedAt,
+      pullRequests: repo.pullRequests.totalCount,
+      mergedPullRequests: repo.mergedPullRequests?.totalCount || 0,
+      issues: repo.issues.totalCount,
+      closedIssues: repo.closedIssues?.totalCount || 0,
+      releases: repo.releases.totalCount,
+    }));
+    const filePath = "./csv/requisito-1.csv";
+    createCsv(formattedData, filePath);
+  } catch (error) {
+    console.error('Erro ao salvar os repositórios no CSV:', error);
+  }
 }
-
-saveRepositoriesToCsv();
